8bdd475cab446cf06f49b9f8420b853b4ae70457
davidoludolapo <davidoluwadolapo@gmail.com>
2022-04-29 09:00:53 +0100

    first commit

---
+++ pages/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+import { client } from '../lib/client'
+import { Product, FooterBanner, HeroBanner} from '../components'
+
+function Home({ products, bannerData}) {
+  return (
+    <>
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]}/>
+  
+      <div className="products-heading">
+        <h2>Best Selling Products</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea</p>
+      </div>
+      <div className="products-container">{products?.map((product) => <Product key={product._id} product={product}/>)}</div>
+
+      <FooterBanner footerBanner={bannerData && bannerData[0]}/>
+    </>
+  );
+
+
+}
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]'
+  const products = await client.fetch(query)
+
+  const bannerQuery = '*[_type == "banner"]'
+  const bannerData = await client.fetch(bannerQuery)
+
+  return {
+    props: {products, bannerData}
+  }
+}
+ 
+export default Home;
